3d7f0460711b5764bea6de647426ffd272d7d49a
Nabby <trishasalubreaquino@gmail.com>
2024-08-06 17:22:17 -0600

    commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,46 @@
 "use client";
-
-
-import React from 'react';
+ 
+import { useEffect, useState } from 'react';
 import Link from 'next/link';
-import Sidebar from './sidebar'; // Adjusted import path to access components directory
-
-export  function HomePage() {
-  return (
+import Sidebar from './sidebar';
+import Loginauth from './authorization/page'; // Adjust path if needed
+ 
+export default function HomePage() {
+  const [token, setToken] = useState(null);
+  const [loading, setLoading] = useState(true);
+ 
+  useEffect(() => {
+    const checkToken = () => {
+      const storedToken = window.localStorage.getItem("token");
+      const hash = window.location.hash;
+ 
+      if (!storedToken && hash) {
+        const _token = new URLSearchParams(hash.substring(1)).get('token');
+        if (_token) {
+          window.localStorage.setItem("token", _token);
+          setToken(_token);
+        }
+        // Clear the hash from the URL
+        window.location.hash = "";
+      } else {
+        setToken(storedToken);
+      }
+      setLoading(false);
+    };
+ 
+    checkToken();
+  }, []);
+ 
+  if (loading) {
+    return <div>Loading...</div>; // Optional loading state
+  }
+ 
+  return !token ? (
+    <Loginauth /> // Render the Login component if there's no token
+  ) : (
     <div className="flex">
       <Sidebar />
       <main className="flex-1 p-4">
-        <h1></h1>
         <nav>
           <ul>
             <li><Link href="/library">Library</Link></li>
@@ -20,13 +50,12 @@ export  function HomePage() {
             <li><Link href="/favorites">Favorites</Link></li>
           </ul>
         </nav>
-        {/* Add content for the main page here */}
       </main>
     </div>
-
   );
-
 }
+ 
+ 
 
 export default function Home() {
   return (
