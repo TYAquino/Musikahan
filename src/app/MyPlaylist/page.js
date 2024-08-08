67283b7c387d6fb6088739baf501d94f5772de6d
Nabby <trishasalubreaquino@gmail.com>
2024-08-08 02:35:58 -0600

    pushing

--- src/app/MyPlaylist/page.js
+++ src/app/MyPlaylist/page.js
@@ -1,26 +1,30 @@
-'use client'; // Add this line at the top
+"use client"; // Add this line at the top
 
-import React, { useEffect, useState } from 'react';
-import apiClient, { setClientToken } from '../axios/page.js'; // Adjust the import path as necessary
+import React, { useEffect, useState } from "react";
+import apiClient, { setClientToken } from "../axios/page.js"; // Adjust the import path as necessary
 
 const MyPlaylist = () => {
   const [playlists, setPlaylists] = useState(null);
   const [error, setError] = useState(null);
-  const token = typeof window !== 'undefined' ? window.localStorage.getItem('access_token') : null;
+  const token =
+    typeof window !== "undefined"
+      ? window.localStorage.getItem("access_token")
+      : null;
 
   useEffect(() => {
     if (token) {
       setClientToken(token);
-      apiClient.get('me/playlists')
-        .then(response => {
+      apiClient
+        .get("me/playlists")
+        .then((response) => {
           setPlaylists(response.data.items);
         })
-        .catch(error => {
-          console.error('Error fetching playlists:', error);
-          setError('Failed to fetch playlists.');
+        .catch((error) => {
+          console.error("Error fetching playlists:", error);
+          setError("Failed to fetch playlists.");
         });
     } else {
-      setError('No authentication token found.');
+      setError("No authentication token found.");
     }
   }, [token]);
 
@@ -28,9 +32,14 @@ const MyPlaylist = () => {
     <div className="flex flex-col h-full bg-black p-4 overflow-y-auto">
       {error && <div className="text-red-500">{error}</div>}
       {playlists ? (
-        <div className="flex flex-wrap gap-4"> {/* Container for inline items */}
+        <div className="flex flex-wrap gap-4">
+          {" "}
+          {/* Container for inline items */}
           {playlists.map((playlist) => (
-            <div key={playlist.id} className="flex flex-col items-center p-2 text-white rounded shadow w-40">
+            <div
+              key={playlist.id}
+              className="flex flex-col items-center p-2 text-white rounded shadow w-40 hover:scale-110 transition duration-300 ease-in-out"
+            >
               {/* Check if playlist.images exists and has at least one image */}
               {playlist.images && playlist.images.length > 0 && (
                 <img
@@ -39,7 +48,9 @@ const MyPlaylist = () => {
                   className="w-32 h-32 object-cover rounded mb-2"
                 />
               )}
-              <div className="text-center">{playlist.name}</div>
+              <div className="text-center overflow-hidden text-ellipsis whitespace-nowrap w-32">
+                {playlist.name}
+              </div>
             </div>
           ))}
         </div>
--- src/app/favorites/page.js
+++ src/app/favorites/page.js
@@ -1,26 +1,30 @@
-'use client'; // Add this line at the top
+"use client"; // Add this line at the top
 
-import React, { useEffect, useState } from 'react';
-import apiClient, { setClientToken } from '../axios/page.js'; // Adjust the import path as necessary
+import React, { useEffect, useState } from "react";
+import apiClient, { setClientToken } from "../axios/page.js"; // Adjust the import path as necessary
 
 const Favorites = () => {
   const [artists, setArtists] = useState(null);
   const [error, setError] = useState(null);
-  const token = typeof window !== 'undefined' ? window.localStorage.getItem('access_token') : null;
+  const token =
+    typeof window !== "undefined"
+      ? window.localStorage.getItem("access_token")
+      : null;
 
   useEffect(() => {
     if (token) {
       setClientToken(token);
-      apiClient.get('me/following?type=artist')
-        .then(response => {
+      apiClient
+        .get("me/following?type=artist")
+        .then((response) => {
           setArtists(response.data.artists.items);
         })
-        .catch(error => {
-          console.error('Error fetching artists:', error);
-          setError('Failed to fetch artists.');
+        .catch((error) => {
+          console.error("Error fetching artists:", error);
+          setError("Failed to fetch artists.");
         });
     } else {
-      setError('No authentication token found.');
+      setError("No authentication token found.");
     }
   }, [token]);
 
@@ -29,18 +33,21 @@ const Favorites = () => {
       {error && <div className="text-red-500">{error}</div>}
       {artists ? (
         <div className="flex flex-wrap gap-4">
-        {artists.map((artist) => (
-          <div key={artist.id} className="flex flex-col items-center p-2 text-white rounded shadow w-40">
-          {artist.images && artist.images.length > 0 && (
+          {artists.map((artist) => (
+            <div
+              key={artist.id}
+              className="flex flex-col items-center p-2 text-white rounded shadow w-40 hover:scale-110 transition duration-300 ease-in-out"
+            >
+              {artist.images && artist.images.length > 0 && (
                 <img
                   src={artist.images[0].url}
                   alt={artist.name}
                   className="w-32 h-32 object-cover rounded mb-2"
                 />
               )}
-          <div className="text-center">{artist.name}</div>
-          </div>
-        ))}
+              <div className="text-center">{artist.name}</div>
+            </div>
+          ))}
         </div>
       ) : (
         !error && <div>Loading artists...</div>
